84f452fe367c8201aefd60a0c35fde56bfaec9e5
Juan Insuasti <insua@Galactus>
2022-06-24 19:11:18 -0500

    initial list component tests

---
+++ foundations/src/components/List/index.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+
+describe('List component', () =>{
+  test('It should render listItems', () => {
+    expect(false).toBeTruthy();
+  });
+});
---
+++ foundations/src/components/ListItem/index.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+describe('ListItem component', () =>{
+  test('It should render a price', () => {
+    expect(false).toBeTruthy();
+  });
+
+  test('It should render a date', () => {
+    expect(false).toBeTruthy();
+  });
+
+  test('It should render a title', () => {
+    expect(false).toBeTruthy();
+  });
+});
